7388b528de5350d9af880e9ae9d3e1620fd947c1
pytharchimede <amani_ulrich@outlook.fr>
2025-02-21 12:07:01 +0000

    Update Menu

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'includes/header.php';
 
 <div class="w-full max-w-7xl bg-white shadow-lg rounded-xl p-8 mt-6">
     <h1 class="text-4xl font-semibold text-gray-900 mb-10 text-center uppercase tracking-wide">Accueil - Gestion de Stock</h1>
-    <p class="text-center">Bienvenue dans la gestion de stock pour BTP.</p>
+    <p class="text-center"></p>
 
 
     <div class="w-full max-w-7xl bg-white shadow-lg rounded-xl p-8 mt-6">
